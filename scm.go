555d3cf62f94cdd49282a200036975159e36fb0b
Thom Boyer <thom@boyers.org>
2018-08-11 20:01:36 -0600

    Move main() to a separate file

--- scm.go
+++ scm.go
@@ -18,20 +18,6 @@ import (
 	"github.com/perlmonger42/LiSP/scan"
 )
 
-func main() {
-	scanner := scan.NewScanner("<stdin>", scan.NewConsoleReader())
-	for err := Repl(scanner); err != nil; err = Repl(scanner) {
-		fmt.Println(err)
-	}
-}
-
-//func main() {
-//	scanner := scan.NewScanner("<str>", strings.NewReader("(list 1)"))
-//	for err := Repl(scanner); err != nil; err = Repl(scanner) {
-//		fmt.Println(err)
-//	}
-//}
-
 /*
  Eval / Apply
 */
